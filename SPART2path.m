3e9fa138933261ddf189e90e1bf0e6d1d6f97f7b
Josep Virgili Llop <jvirgili@nps.edu>
2016-04-25 16:43:19 -0700

    Added utilities folder to the SPART path.

--- SPART2path.m
+++ SPART2path.m
@@ -3,6 +3,7 @@
 %Add all the required folders into the path
 addpath(sprintf('%s/KinDyn',pwd));
 addpath(sprintf('%s/SimulinkLibrary',pwd));
+addpath(sprintf('%s/Utilities',pwd));
 
 %Save path
 savepath;
