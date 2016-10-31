37de9f76e4a7ef298ed370eea35f3bd068136ead
Josep Virgili Llop <jvirgili@nps.edu>
2016-10-30 17:04:15 -0700

    Added full last name.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,8 +49,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SPART'
-copyright = u'2016, Josep Virgili'
-author = u'Josep Virgili'
+copyright = u'2016, Josep Virgili-Llop'
+author = u'Josep Virgili-Llop'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -228,7 +228,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'SPART.tex', u'SPART Documentation',
-     u'Josep Virgili', 'manual'),
+     u'Josep Virgili-Llop', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
