2bd93d9bbd5be4626dab8c7245743b05e4dae7a2
Josep Virgili Llop <jvirgili@nps.edu>
2016-11-03 17:37:03 -0700

    Compacted code when defining quaternion.

--- Utilities/Transformations/Euler_DCM.m
+++ Utilities/Transformations/Euler_DCM.m
@@ -3,9 +3,7 @@ function DCM = Euler_DCM(e,alpha) %#codegen
 %and angle alpha.
 
 %Create quaternion
-q=[ e(1)*sin(alpha/2);
-    e(2)*sin(alpha/2);
-    e(3)*sin(alpha/2);
+q=[ e*sin(alpha/2);
     cos(alpha/2)];
 
 %Convert quaternion to DCM
