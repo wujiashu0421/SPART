c7a2fdf57b1c8632e53ff73b16528e397c45eec7
Josep Virgili Llop <jvirgili@nps.edu>
2016-11-10 17:42:29 -0800

    Used new robot model in the function.

--- KinDyn/I_I.m
+++ KinDyn/I_I.m
@@ -1,12 +1,11 @@
-function [I0,Im]=I_I(R0,RL,data) %#codegen
+function [I0,Im]=I_I(R0,RL,robot) %#codegen
 % Converts the inertias in local frame to inertia in the inertial frame.
 %
 % Input ->
 %   R0 -> Rotation matrix from the base-spacecraft to the inertial frame.
 %   RL -> Links 3x3 rotation matrices.
-%       data.n -> Manipulator number of joints and links.
-%       data.man -> Manipulator data.
-%           data.man(i).I -> Link inertia.
+%   robot -> Robot model
+%
 % Output ->
 %   I0 -> Base-spacecraft inertia in inertial frame.
 %   Im -> Manipulator inertia in inertial frame.
@@ -16,14 +15,14 @@ function [I0,Im]=I_I(R0,RL,data) %#codegen
 %=== CODE ===%
 
 %Base-spacecraft inertia
-I0 = R0*data.base.I;
+I0 = R0*robot.base_link.inertia;
 if not(isempty(coder.target)) %Only use during code generation (allowing symbolic computations)
     %Pre-allocate inertias
-    Im=zeros(3,3,data.n);
+    Im=zeros(3,3,robot.n_links);
 end
 %Inertias of the links
-for i=1:(data.n)
-    Im(1:3,1:3,i)=RL(1:3,1:3,i)*data.man(i).I*RL(1:3,1:3,i)';
+for i=1:(robot.n_links)
+    Im(1:3,1:3,i)=RL(1:3,1:3,i)*robot.links(i).inertia*RL(1:3,1:3,i)';
 end
 
 end
