0bfa090c33af652d7445a7451caf882957896c95
Josep Virgili Llop <jvirgili@nps.edu>
2016-11-11 19:05:52 -0800

    Adapted the tutorial to the new robot model and new functions.

--- Examples/Tutorial/Tutorial.m
+++ Examples/Tutorial/Tutorial.m
@@ -19,7 +19,7 @@ L6=70;
 data.n=5;
 
 %First joint
-data.man(1).type=0;
+data.man(1).type=1;
 data.man(1).DH.d = L1;
 data.man(1).DH.alpha = pi/2;
 data.man(1).DH.a = 0;
@@ -29,7 +29,7 @@ data.man(1).mass=2;
 data.man(1).I=diag([2,1,3])/10;
 
 %Second joint
-data.man(2).type=0;
+data.man(2).type=1;
 data.man(2).DH.d = 0;
 data.man(2).DH.alpha = 0;
 data.man(2).DH.a = sqrt(L2^2+L3^2);
@@ -39,7 +39,7 @@ data.man(2).mass=2;
 data.man(2).I=diag([2,1,3])/10;
 
 %Third joint
-data.man(3).type=0;
+data.man(3).type=1;
 data.man(3).DH.d = 0;
 data.man(3).DH.alpha = 0;
 data.man(3).DH.a =L4;
@@ -49,7 +49,7 @@ data.man(3).mass=2;
 data.man(3).I=diag([2,1,3])/10;
 
 %Fourth joint
-data.man(4).type=0;
+data.man(4).type=1;
 data.man(4).DH.d = 0;
 data.man(4).DH.alpha = pi/2;
 data.man(4).DH.a = 0;
@@ -59,7 +59,7 @@ data.man(4).mass=2;
 data.man(4).I=diag([2,1,3])/10;
 
 %Fifth joint
-data.man(5).type=0;
+data.man(5).type=1;
 data.man(5).DH.d = L5+L6;
 data.man(5).DH.alpha =-pi/2;
 data.man(5).DH.a = 0;
@@ -90,29 +90,31 @@ qm=[0;0;0;0;0];
 q0dot=zeros(6,1); %Base-spacecraft velocity
 qmdot=[4;-1;5;2;1]*pi/180; %Joint velocities
 
+%--- Create robot structure ---%
+[robot] = DH_Serial2robot(data);
+
 %--- Kinematics ---%
-[RJ,RL,r,l,e,g,TEE]=Kinematics_Serial(R0,r0,qm,data);
+[RB,RJ,RL,rB,rJ,rL,e,g]=Kinematics(R0,r0,qm,robot);
 
 %--- Differential Kinematics ---%
 %Differential kinematics
-[Bij,Bi0,P0,pm]=DiffKinematics_Serial(R0,r0,r,e,g,data);
+[Bij,Bi0,P0,pm]=DiffKinematics(R0,r0,rL,e,g,robot);
 %Jacobian of the Link 3
-[J03, Jm3]=Jacob(r(1:3,3),r0,r,P0,pm,3,data.n);
+[J03, Jm3]=Jacob(rL(1:3,3),r0,rL,P0,pm,3,robot);
 %End-effector Jacobian
-[J0EE, JmEE]=Jacob(TEE(1:3,4),r0,r,P0,pm,data.n,data.n);
+[J0EE, JmEE]=Jacob(rL(1:3,end),r0,rL,P0,pm,robot.n_links,robot);
 %Velocities
-[t0,tm]=Velocities_Serial(Bij,Bi0,P0,pm,q0dot,qmdot,data);
-
+[t0,tm]=Velocities(Bij,Bi0,P0,pm,q0dot,qmdot,robot);
 
 %--- Inertia Matrices ---%
 %Inertias in inertial frames
-[I0,Im]=I_I(R0,RL,data);
+[I0,Im]=I_I(R0,RL,robot);
 %Mass Composite Body matrix
-[M0_tilde,Mm_tilde]=MCB_Serial(I0,Im,Bij,Bi0,data);
+[M0_tilde,Mm_tilde]=MCB(I0,Im,Bij,Bi0,robot);
 %Generalized Inertia matrix
-[H0, H0m, Hm] = GIM_Serial(M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,data);
+[H0, H0m, Hm] = GIM(M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,robot);
 %Generalized Convective Inertia matrix
-[C0, C0m, Cm0, Cm] = C_Serial(t0,tm,I0,Im,M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,data);
+[C0, C0m, Cm0, Cm] = C(t0,tm,I0,Im,M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,robot);
 
 %--- Forward Dynamics ---%
 
@@ -120,33 +122,33 @@ qmdot=[4;-1;5;2;1]*pi/180; %Joint velocities
 g=9.8; %[m s-2]
 
 %External forces (includes gravity and assumes z is the vertical direction)
-wF0=[0;0;0;0;0;-data.base.mass*g];
-wFm=[zeros(5,data.n);
-    -data.man(1).mass*g,-data.man(2).mass*g,-data.man(3).mass*g,-data.man(4).mass*g,-data.man(5).mass*g];
+wF0=[0;0;0;0;0;-robot.base_link.mass*g];
+wFm=[zeros(5,robot.n_links);
+    -robot.links(1).mass*g,-robot.links(2).mass*g,-robot.links(3).mass*g,-robot.links(4).mass*g,-robot.links(5).mass*g];
 
 %Joint torques
 tauq0=zeros(6,1);
-tauqm=zeros(data.n,1);
+tauqm=zeros(robot.n_links,1);
 
 %Forward Dynamics
-[q0ddot_FD,qmddot_FD] = FD_Serial(tauq0,tauqm,wF0,wFm,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,data);
+[q0ddot_FD,qmddot_FD] = FD(tauq0,tauqm,wF0,wFm,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,robot);
 
 %--- Inverse Dynamics - Flying ---%
 
 %Accelerations
 q0ddot=zeros(6,1);
-qmddot=zeros(5,1);
+qmddot=zeros(robot.n_links,1);
 
 %Accelerations
-[t0dot,tmdot]=Accelerations_Serial(t0,tm,P0,pm,Bi0,Bij,q0dot,qmdot,q0ddot,qmddot,data);
+[t0dot,tmdot]=Accelerations(t0,tm,P0,pm,Bi0,Bij,q0dot,qmdot,q0ddot,qmddot,robot);
 
 %Inverse Dynamics - Flying base
-[tau0,taum] = ID_Serial(wF0,wFm,t0,tm,t0dot,tmdot,P0,pm,I0,Im,Bij,Bi0,data);
+[tau0,taum] = ID(wF0,wFm,t0,tm,t0dot,tmdot,P0,pm,I0,Im,Bij,Bi0,robot);
 
 %--- Forward Dynamics - Floating ---%
 
 %Accelerations
-qmddot=zeros(5,1);
+qmddot=zeros(robot.n_links,1);
 
 %Inverse Dynamics - Floating Base
-[taum_floating,q0ddot_floating] = Floating_ID_Serial(wF0,wFm,Mm_tilde,H0,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,qmddot,data);
+[taum_floating,q0ddot_floating] = Floating_ID(wF0,wFm,Mm_tilde,H0,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,qmddot,robot);
