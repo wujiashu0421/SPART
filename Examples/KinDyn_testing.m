7c9cd959d23a6bae1732320e9f5b7017c93d5385
Josep Virgili Llop <jvirgili@nps.edu>
2016-11-16 20:46:59 -0800

    Adapted examples to new robot model.

--- Examples/KinDyn_testing.m
+++ Examples/KinDyn_testing.m
@@ -11,7 +11,7 @@ close all
 data.n=2;
 
 %First joint
-data.man(1).type=0;
+data.man(1).type=1;
 data.man(1).DH.d = 0;
 data.man(1).DH.alpha = 0;
 data.man(1).DH.a = 1;
@@ -21,7 +21,7 @@ data.man(1).mass=2;
 data.man(1).I=eye(3)/10;
 
 %Second joint
-data.man(2).type=0;
+data.man(2).type=1;
 data.man(2).DH.d = 0;
 data.man(2).DH.alpha = 0;
 data.man(2).DH.a = 1;
@@ -41,6 +41,10 @@ data.base.T_L0_J1=[eye(3),[1;0;0];zeros(1,3),1];
 data.base.mass=10;
 data.base.I=eye(3);
 
+%--- Compute Robot Model ---%
+[robot] = DH_Serial2robot(data);
+
+%--- Variables ---%
 %Base position
 R0=eye(3);
 r0=[0;0;0];
@@ -70,29 +74,29 @@ tauqm=[0;0];
 profile on
 
 %Kinematics
-[RJ,RL,r,l,e,g,TEE]=Kinematics_Serial(R0,r0,qm,data);
+[RB,RJ,RL,rB,rJ,rL,e,g]=Kinematics(R0,r0,qm,robot);
 %Differential Kinematics
-[Bij,Bi0,P0,pm]=DiffKinematics_Serial(R0,r0,r,e,g,data);
+[Bij,Bi0,P0,pm]=DiffKinematics(R0,r0,rL,e,g,robot);
 %Velocities
-[t0,tm]=Velocities_Serial(Bij,Bi0,P0,pm,q0dot,qmdot,data);
+[t0,tm]=Velocities(Bij,Bi0,P0,pm,q0dot,qmdot,robot);
 %Accelerations
-[t0dot,tmdot]=Accelerations_Serial(t0,tm,P0,pm,Bi0,Bij,q0dot,qmdot,q0ddot,qmddot,data);
+[t0dot,tmdot]=Accelerations(t0,tm,P0,pm,Bi0,Bij,q0dot,qmdot,q0ddot,qmddot,robot);
 %Inertias
-[I0,Im]=I_I(R0,RL,data);
+[I0,Im]=I_I(R0,RL,robot);
 %Inverse Dynamics
-[tau0,taum] = ID_Serial(wF0,wFm,t0,tm,t0dot,tmdot,P0,pm,I0,Im,Bij,Bi0,data);
+[tau0,taum] = ID(wF0,wFm,t0,tm,t0dot,tmdot,P0,pm,I0,Im,Bij,Bi0,robot);
 %Mass Composite Body matrix
-[M0_tilde,Mm_tilde]=MCB_Serial(I0,Im,Bij,Bi0,data);
+[M0_tilde,Mm_tilde]=MCB(I0,Im,Bij,Bi0,robot);
 %Generalized Inertia matrix
-[H0, H0m, Hm] = GIM_Serial(M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,data);
+[H0, H0m, Hm] = GIM(M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,robot);
 %Inverse Dynamics Floating Base
-[taum_floating,q0ddot_floating] = Floating_ID_Serial(wF0,wFm,Mm_tilde,H0,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,qmddot,data);
+[taum_floating,q0ddot_floating] = Floating_ID(wF0,wFm,Mm_tilde,H0,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,qmddot,robot);
 %Forward Dynamics
-[q0ddot_FD,qmddot_FD] = FD_Serial(tauq0,tauqm,wF0,wFm,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,data);
+[q0ddot_FD,qmddot_FD] = FD(tauq0,tauqm,wF0,wFm,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,robot);
 %Generalized Convective Inertia matrix
-[C0, C0m, Cm0, Cm] = C_Serial(t0,tm,I0,Im,M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,data);
+[C0, C0m, Cm0, Cm] = C(t0,tm,I0,Im,M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,robot);
 %End-effector Jacobian
-[J0EE, JmEE]=Jacob(TEE(1:3,4),r0,r,P0,pm,data.n,data.n);
+[J0EE, JmEE]=Jacob(rL(1:3,end),r0,rL,P0,pm,robot.n_q,robot);
 
 %Stop profiling
 profile viewer
--- Examples/Symbolic/Sym_example.m
+++ Examples/Symbolic/Sym_example.m
@@ -24,7 +24,7 @@ m_base = sym('m_base','real');
 I_base = sym('I_base',[3,1],'real');
 
 %First joint
-data.man(1).type=0;
+data.man(1).type=1;
 data.man(1).DH.d = d(1);
 data.man(1).DH.alpha = alpha(1);
 data.man(1).DH.a = a(1);
@@ -34,7 +34,7 @@ data.man(1).mass=m(1);
 data.man(1).I=diag(I(1:3,1));
 
 %Second joint
-data.man(2).type=0;
+data.man(2).type=1;
 data.man(2).DH.d = d(2);
 data.man(2).DH.alpha = alpha(2);
 data.man(2).DH.a = a(2);
@@ -54,9 +54,14 @@ data.base.T_L0_J1=[eye(3),[0;0;0];zeros(1,3),1];
 data.base.mass=m_base;
 data.base.I=diag(I_base(1:3,1));
 
+%--- Create robot structure ---%
+[robot] = DH_Serial2robot(data);
+
+%--- Parameters ---%
+
 %Base position
-R0=eye(3);
-r0=[0;0;0];
+R0=sym(eye(3));
+r0=sym([0;0;0]);
 
 %Joint variables
 qm=sym('qm',[data.n,1],'real');
@@ -83,25 +88,25 @@ tauqm=[0;0];
 profile on
 
 %Kinematics
-[RJ,RL,r,l,e,g,TEE]=Kinematics_Serial(sym(R0),sym(r0),sym(qm),data);
+[RB,RJ,RL,rB,rJ,rL,e,g]=Kinematics(R0,r0,qm,robot);
 %Differential Kinematics
-[Bij,Bi0,P0,pm]=DiffKinematics_Serial(R0,r0,r,e,g,data);
+[Bij,Bi0,P0,pm]=DiffKinematics(R0,r0,rL,e,g,robot);
 %Velocities
-[t0,tm]=Velocities_Serial(Bij,Bi0,P0,pm,q0dot,qmdot,data);
+[t0,tm]=Velocities(Bij,Bi0,P0,pm,q0dot,qmdot,robot);
 %Accelerations
-[t0dot,tmdot]=Accelerations_Serial(t0,tm,P0,pm,Bi0,Bij,q0dot,qmdot,q0ddot,qmddot,data);
+[t0dot,tmdot]=Accelerations(t0,tm,P0,pm,Bi0,Bij,q0dot,qmdot,q0ddot,qmddot,robot);
 %Inertias
-[I0,Im]=I_I(R0,RL,data);
+[I0,Im]=I_I(R0,RL,robot);
 %Inverse Dynamics
-[tau0,taum] = ID_Serial(wF0,wFm,t0,tm,t0dot,tmdot,P0,pm,I0,Im,Bij,Bi0,data);
+[tau0,taum] = ID(wF0,wFm,t0,tm,t0dot,tmdot,P0,pm,I0,Im,Bij,Bi0,robot);
 %Mass Composite Body matrix
-[M0_tilde,Mm_tilde]=MCB_Serial(I0,Im,Bij,Bi0,data);
+[M0_tilde,Mm_tilde]=MCB(I0,Im,Bij,Bi0,robot);
 %Generalized Inertia matrix
-[H0, H0m, Hm] = GIM_Serial(M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,data);
+[H0, H0m, Hm] = GIM(M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,robot);
 %Generalized Convective Inertia matrix
-[C0, C0m, Cm0, Cm] = C_Serial(t0,tm,I0,Im,M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,data);
+[C0, C0m, Cm0, Cm] = C(t0,tm,I0,Im,M0_tilde,Mm_tilde,Bij,Bi0,P0,pm,robot);
 %End-effector Jacobian
-[J0EE, JmEE]=Jacob(TEE(1:3,4),r0,r,P0,pm,data.n,data.n);
+[J0EE, JmEE]=Jacob(rL(1:3,end),r0,rL,P0,pm,robot.n_links,robot);
 
 %Stop profiling
 profile viewer
@@ -112,7 +117,7 @@ return
 %--- Steps that take a very long time to compute ---%
 
 %Inverse Dynamics Floating Base (has an inverse)
-[taum_floating,q0ddot_floating] = Floating_ID_Serial(wF0,wFm,Mm_tilde,H0,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,qmddot,data);
+[taum_floating,q0ddot_floating] = Floating_ID(wF0,wFm,Mm_tilde,H0,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,qmddot,robot);
 %Forward Dynamics
-[q0ddot_FD,qmddot_FD] = FD_Serial(tauq0,tauqm,wF0,wFm,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,data);
+[q0ddot_FD,qmddot_FD] = FD(tauq0,tauqm,wF0,wFm,t0,tm,P0,pm,I0,Im,Bij,Bi0,q0dot,qmdot,robot);
 
