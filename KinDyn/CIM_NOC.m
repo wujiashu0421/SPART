f7e06d9d5aa4b590090aca7911216100e51c2feb
Josep Virgili Llop <jvirgili@nps.edu>
2016-12-18 15:05:58 -0800

    Added two new functions that allow to compute the Natural Orthogonal Complement Matrix time derivative and to compute the Convective Inertia Matrix using the Natural Orthogonal Complement Matrix.

---
+++ KinDyn/CIM_NOC.m
@@ -0,0 +1,78 @@
+function [C]=CIM_NOC(N,Ndot,t0,tL,I0,Im,robot)
+% Computes the Generalized Convective Inertia Matrix of a Serial Manipulator.
+%
+% Input ->
+%   N -> Natural Orthogonal Complement matrix.
+%   N -> Natural Orthogonal Complement matrix time derivative.
+%   t0 -> Inertial twist of the base-spacecraft.
+%   tL -> Inertial twist of the links.
+%   I0 -> Base-spacecraft inertia in inertial frame.
+%   Im -> Manipulator inertia in inertial frame.
+%   robot -> Robot model.
+%
+% Output ->
+%   C -> Generalized convective inertia matrix.
+
+%=== LICENSE ===%
+
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU Lesser General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%=== Code ===%
+
+%--- Generalized mass matrix ---%
+
+%Pre-allocate
+if not(isempty(coder.target)) %Only use during code generation (allowing symbolic computations)
+    %Pre-allocate M
+    M=zeros(6+6*robot.n_links_joints,6+6*robot.n_links_joints);
+end
+
+%Base contribution
+M(1:6,1:6)=[I0(1:3,1:3),zeros(3,3);zeros(3,3),robot.base_link.mass*eye(3)];
+%Manipulator contribution
+for i=1:robot.n_links_joints
+    M(6+6*i-5:6+6*i,6+6*i-5:6+6*i)=[Im(1:3,1:3,i),zeros(3,3);zeros(3,3),robot.links(i).mass*eye(3)];
+end
+
+%--- Omega ---%
+%Base-spacecraft Omega
+Omega0=[SkewSym(t0(1:3)), zeros(3,3);
+    zeros(3,3), SkewSym(t0(1:3))];
+if not(isempty(coder.target)) %Only use during code generation (allowing symbolic computations)
+    %Pre-allocate Omega
+    Omega=zeros(6,6,n);
+end
+%Compute Omega
+for j=1:i
+    Omega(1:6,1:6,j)=[SkewSym(tL(1:3,j)), zeros(3,3);
+        zeros(3,3), SkewSym(tL(1:3,j))];
+end
+
+%--- Mdot ---%
+if not(isempty(coder.target)) %Only use during code generation (allowing symbolic computations)
+    %Pre-allocate
+    Mdot=zeros(6+6*robot.n_links_joints,6+6*robot.n_links_joints);
+end
+%Base-spacecraft Mdot
+Mdot(1:6,1:6)=[Omega0(1:3,1:3)*I0, zeros(3,3); zeros(3,3), zeros(3,3)];
+%Compute Mdot
+for i=1:robot.n_links_joints
+    Mdot(6+6*i-5:6+6*i,6+6*i-5:6+6*i)=[Omega(1:3,1:3,i)*Im(1:3,1:3,i), zeros(3,3); zeros(3,3), zeros(3,3)];
+end
+
+%--- Convective Inertia Matrix ---%
+C=N'*(M*Ndot+Mdot*N);
+
+
+end
---
+++ KinDyn/NOCdot.m
@@ -0,0 +1,51 @@
+function [Ndot] = NOCdot(r0,t0,rL,tL,P0,pm,robot)
+% Computes the Natural Orthogonal Complement (NOC) matrix time derivative
+%
+% Input ->
+%   r0 -> Inertial position of the base-spacecraft.
+%   t0 -> Inertial twist of the base-spacecraft.
+%   rL -> Links inertial positions.
+%   tL -> Inertial twist of the links.
+%   P0 -> Base-spacecraft twist-propagation vector.
+%   pm -> Manipulator twist-propagation vector.
+%   robot -> Robot model.
+%
+% Output ->
+%   Ndot -> Natural Orthogonal Complement (NOC) matrix time derivative
+
+%=== LICENSE ===%
+
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU Lesser General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%=== Code ===%
+
+%Pre-allocate
+if not(isempty(coder.target)) %Only use during code generation (allowing symbolic computations)
+    %Pre-allocate NOCdot
+    Ndot=zeros(6*robot.n_links_joints,6+robot.n_q);
+end
+
+%Compute the NOC time derivative matrix by using the Jacobians time derivative
+%Base-spacecraft contribution
+Omega0=[SkewSym(t0(1:3)), zeros(3,3);
+    zeros(3,3), SkewSym(t0(1:3))];
+Ndot(1:6,1:6+robot.n_q)=[Omega0*P0,zeros(6,robot.n_links_joints)];
+%Manipulator contribution
+for i=1:robot.n_links_joints
+    [J0doti, Jmdoti]=Jacobdot(rL(1:3,i),tL(1:6,i),r0,t0,rL,tL,P0,pm,i,robot);
+    Ndot(6+6*i-5:6+6*i,1:6+robot.n_q)=[J0doti,Jmdoti];
+end
+
+
+end
