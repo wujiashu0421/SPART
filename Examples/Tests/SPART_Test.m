3ac1b5164ce4129b33fde98d45860bf15820f198
Josep Virgili Llop <jvirgili@nps.edu>
2016-12-19 11:24:20 -0800

    Expanded the testing to serial URDF and multi-arm URDF.

--- Examples/Tests/SPART_Test.m
+++ Examples/Tests/SPART_Test.m
@@ -3,10 +3,10 @@
 clc
 clear
 
-disp('Testing the different functions of SPART:')
+disp('SPART - Serial DH Test:')
 
 %--- Load robot model ---%
-[robot,Variables] = Load_SerialRobot();
+[robot,Variables] = Load_SerialDHRobot();
 
 %--- Tests ---%
 %Inertia matrices test
@@ -15,3 +15,36 @@ IM_Test(robot,Variables);
 ID_Test(robot,Variables);
 %Forward Dynamics test
 FD_Test(robot,Variables);
+
+fprintf('\n');
+disp('SPART - Serial URDF Test:')
+
+%--- Load robot model ---%
+filename='../URDF_Tutorial/kuka_iiwa/kuka_iiwa.urdf';
+[robot,Variables] = Load_SerialURDFRobot(filename);
+
+%--- Tests ---%
+%Inertia matrices test
+IM_Test(robot,Variables);
+%Inverse Dynamics test
+ID_Test(robot,Variables);
+%Forward Dynamics test
+FD_Test(robot,Variables);
+
+
+fprintf('\n');
+disp('SPART - Multibranch (from base) URDF Test:')
+
+%--- Load robot model ---%
+filename='../URDF_Tutorial/Multi_kuka_iiwa/Two_iiwa_Base.urdf';
+[robot,Variables] = Load_SerialURDFRobot(filename);
+
+%--- Tests ---%
+%Inertia matrices test
+IM_Test(robot,Variables);
+%Inverse Dynamics test
+ID_Test(robot,Variables);
+%Forward Dynamics test
+FD_Test(robot,Variables);
+
+
