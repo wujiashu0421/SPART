83ec3373dcb0806fa6cb0fbf132f1b86b05fe4f3
Josep Virgili Llop <jvirgili@nps.edu>
2017-03-21 17:33:49 -0700

    Added documentation.

--- src/attitude_transformations/Angles321_DCM.m
+++ src/attitude_transformations/Angles321_DCM.m
@@ -1,5 +1,34 @@
-function DCM = Angles321_DCM(Angles) %#codegen
-%Convert the Euler angles (321 sequence), x-phi, y-theta, z-psi to DCM.
+function DCM = Angles321_DCM(Angles)
+% Convert the Euler angles (321 sequence), x-phi, y-theta, z-psi to its DCM equivalent.
+%
+% DCM = Angles321_DCM(Angles)
+%
+% :parameters: 
+%   * Angles -- Euler angles [x-phi, y-theta, z-psi] -- [3x1].
+%
+% :return: 
+%   * DCM -- Direction Cosine Matrix -- [3x3].
+%
+% See also: :func:`src.attitude_transformations.Angles123_DCM` and :func:`src.attitude_transformations.DCM_Angles321`.
+
+%{  
+    LICENSE
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
+%=== CODE ===%
 
 phi = Angles(1);
 theta = Angles(2);
