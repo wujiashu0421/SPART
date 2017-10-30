faa76f4dbaf0b69bb3e75a16cb5b0e2b8a5cad7b
Josep Virgili Llop <jvirgili@nps.edu>
2017-10-29 18:42:25 -0700

    Corrected connectivity map (branch connectivity). Now the possibility of a branching event in the middle of a branch is correctly considered.

--- src/robot_model/ConnectivityMap.m
+++ src/robot_model/ConnectivityMap.m
@@ -3,17 +3,17 @@ function [branch,child,child_base]=ConnectivityMap(robot)
 %
 % [branch,child,child_base]=ConnectivityMap(robot)
 %
-% :parameters: 
+% :parameters:
 %   * robot -- Robot model (see :doc:`/Robot_Model`).
 %
-% :return: 
+% :return:
 %   * Branch -- Branch connectivity map.
 %   * child -- Link--child map.
 %   * child_base -- Base--link child map.
 %
 % See also: :func:`src.robot_model.urdf2robot` and :func:`src.robot_model.DH_Serial2robot`.
 
-%{  
+%{
     LICENSE
 
     This program is free software: you can redistribute it and/or modify
@@ -37,13 +37,16 @@ branch=zeros(robot.n_links_joints,robot.n_links_joints);
 
 %Populate Branch connectivity map
 for i=robot.n_links_joints:-1:1
-    for j=i:-1:1
-        branch(i,j)=1;
-        if robot.joints(robot.links(j).parent_joint).parent_link==0
-            
+    last_parent_link=i;
+    branch(i,i)=1;
+    %Descent through the branch until the base, populating the connectivity map
+    while true
+        parent_link=robot.joints(robot.links(last_parent_link).parent_joint).parent_link;
+        if parent_link==0
             break
         end
-
+        branch(i,parent_link)=1;
+        last_parent_link = parent_link;
     end
 end
 
